27ede830d1d694d340f753d6a2284c3567508e3e
anohk <anyoung.noh@gmail.com>
2017-06-05 02:17:51 +0900

    데이터의 정렬기준을 createDate로 변경하고, 역순 조회를 정순 조회로 변경
    createDate 에는 MAX_SAFE_INTEGER - new Date().getTime() 값을 저장한다.
    최근에 저장된 데이터일 수록 createDate의 값은 작다.
    데이터의 정렬기준을 createDate로 놓게되면, 데이터를 역순으로 조회하지 않고도
    최신 데이터부터 조회할 수 있다.

--- public/js/loadBookList.js
+++ public/js/loadBookList.js
@@ -1,5 +1,6 @@
 var isLoading = false;
 var firstKey, prevKey, dataLen;
+var lastCreateDate = 0;
 var pagenumber = 0;
 
 
@@ -14,83 +15,56 @@ $(window).scroll(function(){
 function loadBookList(){
   // loadBookList가 실행되고 있는 도중에는 스크롤 이벤트 발생하지 않음
   isLoading = true;
-  var bookshelfRef = database.ref('bookshelf/').orderByKey();
+  var bookshelfRef = database.ref('bookshelf/').orderByChild('createDate');
 
-  if (firstKey) {
-    // 만약 마지막 키 값이 있다면 21개의 데이터 호출 : 이후 중복 체크해서 제거해야함
-    // 데이터의 시작을 firstKey 보다 작거나 같은 값으로 변경
-    bookshelfRef = bookshelfRef.limitToLast(21).endAt(firstKey);
-    prevKey = firstKey;
-    firstKey = null;
-    pagenumber += 1;
-
+  if (lastCreateDate) {
+    // 만약 lastCreateDate가 있다면 21개의 데이터 호출 : 이후 중복 체크해서 제거해야함
+    // 데이터는 createDate(정렬기준)가 lastCreateDate 보다 같거나 큰 값을 갖는 것 부터 시작
+    bookshelfRef = bookshelfRef.limitToFirst(21).startAt(lastCreateDate);
   } else {
-    // 처음에는 데이터의 마지막에서 20개의 데이터 호출
-    // console.log('first');
-    bookshelfRef = bookshelfRef.limitToLast(20)
+    // 처음에는 데이터의 처음부터 20개의 데이터 호출
+    bookshelfRef = bookshelfRef.limitToFirst(20)
   }
-
-  bookshelfRef.on('value', function(dataList){
-    dataLen = dataList.val()
-    console.log(Object.keys(dataLen).length);
-  });
-
-  // TODO 마지막 데이터가 호출 된 후, 함수를 종료해야한다.
-  // 현재 스크롤을 끝까지 내리면, 데이터를 더 불러 오지 않지만,
-  // 스크롤을 위로 올렸다가 다시 내리면 처음 데이터부터 다시 순회하며 출력하고 있음.
-  if ( dataLen == 1 ){
-    return;
-  } else {
-    $(".bookshelf").append("<div class=\"prepend" + pagenumber + "\"></div>");
-    bookshelfRef.on('child_added', on_child_added);
-    bookshelfRef.on('child_changed', on_child_changed);
-  }
-
+  bookshelfRef.on('child_added', on_child_added);
+  bookshelfRef.on('child_changed', on_child_changed);
 }
 
 function on_child_added(data) {
-  // console.log(data);
+
   // 데이터 중복 체크
-  // prevKey = 이전 loadBookList를 실행했을 때의 firstKey
-  // prevKey와 일치하는 data.key를 가진 data는 이미 사용했기 때문에 중복 제거
-  if (data.key == prevKey) {
+  var bookData = data.val();
+  var createDate = bookData.createDate;
+  if (createDate == lastCreateDate) {
     isLoading = false;
     return;
   }
 
   var key = data.key
-  var bookData = data.val();
   var title = bookData.title;
   var publisher = bookData.publisher;
   var html =
   "<div class=\"book-container\" id=\"" + key + "\">" +
-
-    "<div class=\"btns\">" +
-    "<div class=\"edit\">" +
-    "<a onclick=\"editData('"+key+"')\"><i class=\"material-icons\">mode_edit</i></a>" +
-    "</div>" +
-    "<div class=\"remove\" >" +
-    "<a onclick=\"deleteData('"+key+"')\"><i class=\"material-icons\">delete</i></a>" +
-    "</div>" +
-    "</div>" +
-
+  "<div class=\"btns\">" +
+  "<div class=\"edit\">" +
+  "<a onclick=\"editData('"+key+"')\"><i class=\"material-icons\">mode_edit</i></a>" +
+  "</div>" +
+  "<div class=\"remove\" >" +
+  "<a onclick=\"deleteData('"+key+"')\"><i class=\"material-icons\">delete</i></a>" +
+  "</div>" +
+  "</div>" +
   "<ul class=\"book-list\">" +
-
-    "<li class=\"book-title origin-data\">" + title + "</li>" +
-    "<input type=\"text\" name=\"title\" class=\"editData editTitle\">"+
-    "<li class=\"publisher origin-data\">" + publisher + "</li>" +
-    "<input type=\"text\" name=\"publisher\" class=\"editData editPublisher\">"+
-    "<button type=\"button\" name=\"button\" onclick=\"saveData('"+key+"')\" class=\"input-btn\">save</button>" +
-    "<button type=\"button\" name=\"button\" onclick=\"hideInputForm()\" class=\"input-btn\">cancel</button>"+
-
+  "<li class=\"book-title origin-data\">" + title + "</li>" +
+  "<input type=\"text\" name=\"title\" class=\"editData editTitle\">"+
+  "<li class=\"publisher origin-data\">" + publisher + "</li>" +
+  "<input type=\"text\" name=\"publisher\" class=\"editData editPublisher\">"+
+  "<button type=\"button\" name=\"button\" onclick=\"saveData('"+key+"')\" class=\"input-btn\">save</button>" +
+  "<button type=\"button\" name=\"button\" onclick=\"hideInputForm()\" class=\"input-btn\">cancel</button>"+
   "</ul>" +
-
   "</div>";
-  $(".prepend"+pagenumber).prepend(html)
 
-  if ( firstKey == null  ) {
-    firstKey = data.key
-  }
+  $('.bookshelf').append(html)
+
+  lastCreateDate = createDate;
   isLoading = false;
 }
 
@@ -99,13 +73,7 @@ function on_child_changed(data) {
   var data = data.val();
   var title = data.title;
   var publisher = data.publisher;
-  console.log(data);
-  console.log(title);
-  console.log(publisher);
 
-  // $('#'+key).find('.book-title').text(title)
-  // $('#'+key).find('.publisher').text(publisher)
-  
   $('#'+key+ " > ul.book-list > li.book-title").text(title);
   $('#'+key+ " > ul.book-list > li.publisher").text(publisher);
 }
